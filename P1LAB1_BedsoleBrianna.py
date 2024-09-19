fde0bbd95ab71e6db012e1e31184d799f6c32446
BriannaBedsole <bedsoleb2310@student.faytechcc.edu>
2024-09-19 16:24:26 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_BedsoleBrianna.py
@@ -0,0 +1,8 @@
+#Brianna Bedsole
+#9/18/2024
+#P1LAB1
+#In this project we use basic python commands such as input and print to create a program. Our commands should allow us to enter our first name then last, along with a message that displays the first name and last name entered.
+
+first_name = input("Enter Your First Name: ")
+last_name = input("Enter Your Last Name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
